0026e5fa8bf7eb7c76a92ab53ad4ff1027d9f251
Matt Camp <matt.camp@siriusxm.com>
2016-11-12 17:20:50 -0500

    more small updates

--- app/scripts/monty-hall-2.js
+++ app/scripts/monty-hall-2.js
@@ -9,7 +9,11 @@ doors
 	door.prize
 	doors.open
 	doors.userPicked
+	
 
+	utilities:
+		pick unpicked and unprized doors
+		
 
 
 
@@ -49,6 +53,10 @@ game.step[2]
 
 /*
 
+Just run it with jQuery, then ask how to do it with angular. 
+
+actually look at 
+
 I can do this with classes and run everything through jquery, that's the easiest thing to do.
 The model lives in the DOM. 
 
@@ -59,3 +67,123 @@ doors
 
 
 */
+
+
+
+
+;(function(window){
+
+
+	 function MontyGame(el,options) {
+        this.el = el;
+        this.options = extend({}, this.options);
+        extend(this.options, options);
+        this._init();
+    }
+
+
+
+
+    MontyGame.prototype.gameOptions {
+
+    	doorCount: 3,
+    	prizeCount: 1,
+    	userPickCount: 1,
+    	hostPickCount: 1,
+    	doorElement: '<li class="door"></li>',
+    	onStep = function(step) {
+    		return false;
+    	},
+
+    	reset = function() {
+
+    	}
+
+    }
+
+
+    MontyGame.prototype._init() {
+
+
+    	this.addDoors();
+    	this.addPrizes()
+    	this._initEvents();
+    }
+
+
+
+    MontyGame.prototype.addDoors() {
+
+    		var markup = "";
+
+    	for (var i = 0; i < this.gameOptions.doorCount; i++) {
+    			
+    		markup += this.gameOptions.doorElement;
+    	};
+
+
+    }
+
+
+    MontyGame.prototype.addPrizes() {
+
+    	for (var i = 0; i < this.gameOptions.prizeCount; i++) {
+    		
+    	};
+
+    }
+
+    MontyGame.prototype.openDoor() {
+
+    	
+    }
+
+
+    MontyGame.prototype.nextStep() {
+
+    	
+    }
+
+    MontyGame.prototype.hostOpensDoors() {
+
+    }
+
+    MontyGame.prototype.userPicksDoors() {
+
+    }
+
+    MontyGame.prototype.userOpensDoors() {
+    	
+    }
+
+     MontyGame.prototype._initEvents() {
+    	
+    }
+
+
+    var gameSteps = [];
+
+    gameSteps[0] = function() {
+    	this.userPicksDoors();
+
+    }
+
+    gameSteps[1] = function() {
+    	this.hostOpensDoors();
+  	}
+
+    gameSteps[2] = function() {
+    	this.userOpensDoors();
+    }
+
+
+    MontyGame.prototype.gameSteps = gameSteps;
+
+
+
+
+
+window.MontyGame = MontyGame;
+
+
+})(window);
